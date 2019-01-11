054665e5b7406e404843cd6288360598b648b1cf
weverkaj <weverkaj@gmail.com>
2019-01-10 17:15:51 -0800

    Initial commit

---
+++ R/cleanup_veg.R
@@ -0,0 +1,19 @@
+#' @title Cleanup Vegetation Data
+#'
+#' @description Some functions that clean up Vegetation data and put it in a format for analysis
+#'
+#' @param
+#'
+#'
+#' @return
+#'
+#' @examples
+#'
+#' @export
+#'
+#'
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
